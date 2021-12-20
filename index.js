a65f1ceaf422de93c2e210972e168640e0ec5c32
pioupia <pioupiadeux@gmail.com>
2021-12-20 20:13:44 +0100

    Changes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express"),
     app = express(),
-    websocket = require("./Websocket");
+    websocket = require("./Websocket"),
+    path = require("path");
 
 const ws = new websocket();
 ws.listen();
 
-let sessions = [];
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile("./pages/index.html", {
+        root: path.join(__dirname, './')
+    })
+})
+
 
 app.listen(3000);
